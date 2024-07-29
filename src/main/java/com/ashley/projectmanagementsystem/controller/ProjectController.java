e3d960752c06ba83e01b6c6a2711ae76deb0e0db
Ashish23jun <pandey.k.ashish.86@gmail.com>
2024-07-29 15:13:44 +0530

    New

--- src/main/java/com/ashley/projectmanagementsystem/controller/ProjectController.java
+++ src/main/java/com/ashley/projectmanagementsystem/controller/ProjectController.java
@@ -1,5 +1,64 @@
 package com.ashley.projectmanagementsystem.controller;
 
+import com.ashley.projectmanagementsystem.Model.Project;
+import com.ashley.projectmanagementsystem.Model.User;
+import com.ashley.projectmanagementsystem.service.ProjectService;
+import com.ashley.projectmanagementsystem.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/project")
 public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<Project>> getProjects(
+            @RequestParam(required = false) String category,
+            @RequestParam(required = false) String tag,
+            @RequestParam("Authorization") String jwt) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+        List<Project> projects = projectService.getProjectByTeam(user, category, tag);
+        return new ResponseEntity<>(projects, HttpStatus.OK);
+    }
+
+    @GetMapping("/{projectId}")
+    public ResponseEntity<Project> getProjectById(
+            @PathVariable Long projectId,
+            @RequestParam("Authorization") String jwt) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+        Project project = projectService.getProjectById(projectId);
+        return new ResponseEntity<>(project, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Project>createProject(
+            @PathVariable Long projectId,
+            @RequestParam("Authorization") String jwt,
+            @RequestBody Project project) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+        Project createdProject = projectService.createProject(project, user);
+        return new ResponseEntity<>(createdProject, HttpStatus.OK);
+
+    }
+
+@PostMapping("/{projectId}")
+    public ResponseEntity<Project>updateProject(
+            @PathVariable Long projectId,
+            @RequestParam("Authorization") String jwt,
+            @RequestBody Project project) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+        Project updateProject = projectService.updateProject(project, projectId);
+        return new ResponseEntity<>(updateProject, HttpStatus.OK);
+
+    }
 
 }
